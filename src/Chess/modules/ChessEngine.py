61afaeae38b405b5f7ba027810252d03c55c63f4
stalwartBunny <constructingintelligence@gmail.com>
2022-06-01 16:14:14 -0500

    fixed the erasing pieces exploit, created the framework for implementing valid-Move-checker

--- src/Chess/modules/ChessEngine.py
+++ src/Chess/modules/ChessEngine.py
@@ -18,11 +18,48 @@ class GameState():
         self.moveLog = []
 
     def makeMove(self, move):
-        self.board[move.startRow][move.startCol] = '--'
-        self.board[move.endRow][move.endCol] = move.pieceMoved
-        self.moveLog.append(move)
-        self.whiteToMove = not self.whiteToMove
+            if self.board[move.startRow][move.startCol] != '--':
+                self.board[move.startRow][move.startCol] = '--'
+                self.board[move.endRow][move.endCol] = move.pieceMoved
+                self.moveLog.append(move)
+                self.whiteToMove = not self.whiteToMove
 
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+
+    def getAllPossibleMoves(self): #memorizable function for moving through a 2d list
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if(turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+                #    elif piece == 'B':
+                #        self.getBishopMoves(r, c, moves)
+                #    elif piece == 'N':(r, c, moves)
+                #        self.getKnightMoves(r, c, moves)
+                #    elif piece == 'Q':(r, c, moves)
+                #        self.getQueenMoves(r, c, moves)
+                #    elif piece == 'K':(r, c, moves)
+                #        self.getKingMoves(r, c, moves)
+        return moves
+
+        def getPawnMoves(self, r, c, moves):
+            pass
+
+        def getRookMoves(self, r, c, moves):
+            pass
 class Move():
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
     #I thought I escaped keys to values by running away from Java. I should have known better.
@@ -37,6 +74,13 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 1000 + self.endRow * 10 + self.endCol * 10
+        print(self.moveID)
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
